c87ab0c013013faadd2302eabdb0812dd7bf72be
wxb <1213422461@qq.com>
2019-06-19 08:58:31 +0800

    皮肤切换

--- IrisSkinsApply/IrisSkinsApply.Designer.cs
+++ IrisSkinsApply/IrisSkinsApply.Designer.cs
@@ -28,20 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.skinEngine = new Sunisoft.IrisSkin.SkinEngine();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.SkinsChange = new System.Windows.Forms.ToolStripMenuItem();
+            this.SkinsChangeSub = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // skinEngine
+            // 
+            this.skinEngine.@__DrawButtonFocusRectangle = true;
+            this.skinEngine.DisabledButtonTextColor = System.Drawing.Color.Gray;
+            this.skinEngine.DisabledMenuFontColor = System.Drawing.SystemColors.GrayText;
+            this.skinEngine.InactiveCaptionColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.skinEngine.SerialNumber = "";
+            this.skinEngine.SkinFile = null;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SkinsChange});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(785, 25);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // SkinsChange
+            // 
+            this.SkinsChange.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SkinsChangeSub});
+            this.SkinsChange.Name = "SkinsChange";
+            this.SkinsChange.Size = new System.Drawing.Size(68, 21);
+            this.SkinsChange.Text = "IrisSkins";
+            // 
+            // SkinsChangeSub
+            // 
+            this.SkinsChangeSub.Name = "SkinsChangeSub";
+            this.SkinsChangeSub.Size = new System.Drawing.Size(152, 22);
+            this.SkinsChangeSub.Text = "皮肤切换";
             // 
             // IrisSkinsApply
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(785, 491);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
             this.Name = "IrisSkinsApply";
             this.Text = "IrisSkinsApply";
+            this.Load += new System.EventHandler(this.IrisSkinsApply_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Sunisoft.IrisSkin.SkinEngine skinEngine;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem SkinsChange;
+        private System.Windows.Forms.ToolStripMenuItem SkinsChangeSub;
     }
 }
 
--- IrisSkinsApply/IrisSkinsApply.cs
+++ IrisSkinsApply/IrisSkinsApply.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace IrisSkinsApply
 {
     public partial class IrisSkinsApply : Form
     {
+        List<string> Skins;     //皮肤路径列表
+        String SkinsName;      //皮肤文件名
         public IrisSkinsApply()
         {
             InitializeComponent();
+            this.skinEngine.SkinFile = "Skins/SteelBlue.ssk";       //皮肤初始化（只加载一个皮肤）
+        }
+
+        private void IrisSkinsApply_Load(object sender, EventArgs e)
+        {
+            //获取所有皮肤列表的路径
+            Skins = Directory.GetFiles(System.Environment.CurrentDirectory + "\\Skins\\", "*.ssk").ToList();
+            Skins.ForEach(x =>
+            {
+                SkinsName = Path.GetFileNameWithoutExtension(x);    //获取皮肤的文件名
+                SkinsChangeSub.DropDownItems.Add(new ToolStripMenuItem(SkinsName));     //在皮肤菜单选项中添加子菜单
+                SkinsChangeSub.DropDownItems[SkinsChangeSub.DropDownItems.Count - 1].Click += new EventHandler(change_skin_Click);     //给子菜单添加点击事件
+            });
+        }
+        private void change_skin_Click(object sender, EventArgs e)
+        {
+            var _this = this;
+            Skins.ForEach(x =>
+            {
+                SkinsName = Path.GetFileNameWithoutExtension(x);    //获取文件名
+                if (SkinsName == ((System.Windows.Forms.ToolStripDropDownItem)sender).Text)
+                {
+                    _this.skinEngine.SkinFile = x;  //将路径应用到skinEngine实现换肤
+                    ((ToolStripMenuItem)sender).Checked = true;     //将选中的ToolStripMenuItem设置为true    
+                }
+            });
         }
     }
 }
